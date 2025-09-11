8c0b0bfe5dd8e491aecf8d31e7a1717a81d587f3
amburgao <amburgao@users.noreply.github.com>
2025-09-11 15:58:18 -0300

    chore: update readme.md

--- src/leaguewizard/exceptions.py
+++ src/leaguewizard/exceptions.py
@@ -6,5 +6,5 @@ from typing import Any
 class LeWizardGenericError(Exception):
     """Base custom exception error for LeagueWizard."""
 
-    def __init__(self, *args: Any) -> None:  # noqa: D107
+    def __init__(self, *args: Any) -> None:
         super().__init__(*args)
