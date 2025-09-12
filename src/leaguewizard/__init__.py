9cacd9633c8e1ac8a58d419d3ebda52a6ffe2b75
amburgao <amburgao@users.noreply.github.com>
2025-09-11 22:34:55 -0300

    fix: improve leaguewizard process lock
    - ensure only one instance of leaguewizard can run at the same time
    - improve LeWizardGenericError

--- src/leaguewizard/__init__.py
+++ src/leaguewizard/__init__.py
@@ -2,23 +2,18 @@
 
 import asyncio
 import os
-import sys
 import tempfile
 import threading
 import urllib
-import urllib.request
 from pathlib import Path
 from typing import Any
 
 import pystray
-import win32api
-import win32event
-import win32security
-import winerror
 from loguru import logger
 from PIL import Image
 
 from leaguewizard.core import start
+from leaguewizard.exceptions import LeWizardGenericError
 
 Path("logs").mkdir(parents=True, exist_ok=True)
 logger.add("logs/leaguewiz_log.log", rotation="1 MB")
@@ -42,34 +37,20 @@ def to_tray() -> Any:
     )
 
 
-def already_running_error() -> None:
-    """Keep only one process running at system.
-
-    Raises:
-        RuntimeWarning: Another instance is already running error.
-    """
-    try:
-        import ctypes
-
-        ctypes.windll.user32.MessageBoxW(
-            0,
-            "Another instance is already running. Close it to create a new one.",
-            "Warn!",
-            48,
-        )
-        raise RuntimeWarning("Another instance is already running.")
-    except RuntimeWarning:
-        sys.exit(1)
-
-
 def main() -> None:
     """LeagueWizard main entry point function."""
-    win32event.CreateMutex(
-        win32security.SECURITY_ATTRIBUTES(), False, "leaguewizardlock"
-    )
-    last_error = win32api.GetLastError()
-    if last_error == winerror.ERROR_ALREADY_EXISTS:
-        already_running_error()
+    import socket
+
+    s = socket.socket()
+    try:
+        s.bind(("127.0.0.1", 54321))
+    except OSError as e:
+        raise LeWizardGenericError(
+            message="Another instance is already running",
+            show=True,
+            title="Error!",
+            exit=True,
+        ) from e
 
     tray = to_tray()
     tray_thread = threading.Thread(target=tray.run, daemon=True)
--- src/leaguewizard/exceptions.py
+++ src/leaguewizard/exceptions.py
@@ -1,10 +1,18 @@
 """Exceptions module for LeWizard."""
 
-from typing import Any
+import sys
 
 
 class LeWizardGenericError(Exception):
     """Base custom exception error for LeagueWizard."""
 
-    def __init__(self, *args: Any) -> None:
-        super().__init__(*args)
+    def __init__(
+        self, message: str, show: bool = False, title: str = "", exit: bool = False
+    ) -> None:
+        super().__init__(message)
+        if show:
+            from tkinter import messagebox
+
+            messagebox.showerror(title=title, message=message)
+        if exit:
+            sys.exit()
